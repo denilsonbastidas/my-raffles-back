71c6939d1d275d068d7816f85904576a0a86d935
denilson bastidas <79290488+denilsonbastidas@users.noreply.github.com>
2025-03-03 11:06:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const nodemailer = require("nodemailer");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/ticketsDB";
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://rifasdenilsonbastidas:x6PmHulZV28FjKfz@clusterrifas.oi7nx.mongodb.net/";
 
 // 📌 Conectar a MongoDB
 mongoose
