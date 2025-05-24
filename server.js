94d6ffac3730d29d1892b9be5128944417371fed
Denilson <denilson@socialgest.net>
2025-05-24 16:57:57 -0400

    added filtered in paymentMethod

--- server.js
+++ server.js
@@ -618,10 +618,39 @@ app.put("/api/tickets/update-contact/:id", async (req, res) => {
 });
 
 
-// 📌 Endpoint para obtener todos los tickets
+// // 📌 Endpoint para obtener todos los tickets
+// app.get("/api/tickets", async (req, res) => {
+//   try {
+//     const filter = req.query.status === "all" ? {} : { approved: false };
+
+//     const tickets = await Ticket.find(filter).sort({ createdAt: 1 });
+
+//     const ticketsWithImageURL = tickets.map((ticket) => ({
+//       ...ticket._doc,
+//       voucher: ticket.voucher
+//         ? `${req.protocol}://${req.get("host")}/uploads/${ticket.voucher}`
+//         : null,
+//     }));
+
+//     res.json(ticketsWithImageURL);
+//   } catch (error) {
+//     console.error("Error al obtener tickets:", error);
+//     res.status(500).json({ error: "Error al obtener los tickets" });
+//   }
+// });
+
+// 📌 Endpoint para obtener todos los tickets con filtros
 app.get("/api/tickets", async (req, res) => {
   try {
-    const filter = req.query.status === "all" ? {} : { approved: false };
+    const { status, paymentMethod } = req.query;
+
+    // Filtro base según el status
+    let filter = status === "all" ? {} : { approved: false };
+
+    // Si viene paymentMethod en la query, lo agregamos al filtro
+    if (paymentMethod) {
+      filter.paymentMethod = paymentMethod;
+    }
 
     const tickets = await Ticket.find(filter).sort({ createdAt: 1 });
 
@@ -638,6 +667,7 @@ app.get("/api/tickets", async (req, res) => {
     res.status(500).json({ error: "Error al obtener los tickets" });
   }
 });
+
 
 // endpoint para saber si el boleto existe
 app.get("/api/tickets/check", async (req, res) => {
