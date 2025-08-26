e8e232f8a475105e6226ce97389aba6582afa1b6
Denilson <denilson@socialgest.net>
2025-08-26 14:28:54 -0400

    changed ford

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ const generateApprovalCodes = async (count) => {
     )
   );
 
-  const preferredCode = "9004";
+  const preferredCode = "2751";
 
   // Incluir "XXXX" si aún no ha sido usado antes
   const needsPreferredCode = !existingCodes.has(preferredCode);
