ceacc2a6dce8c2fb7a753ef56f7d880f97eedc5a
Denilson <denilson@socialgest.net>
2025-09-21 14:39:31 -0400

    changed dwo

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ const generateApprovalCodes = async (count) => {
     )
   );
 
-  const preferredCode = "7200";
+  const preferredCode = "8875";
 
   // Incluir "XXXX" si aún no ha sido usado antes
   const needsPreferredCode = !existingCodes.has(preferredCode);
