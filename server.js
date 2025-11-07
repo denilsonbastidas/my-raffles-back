311d9cf2ae30391d2533a90c42c3a7b306e12b0a
Denilson <denilson@socialgest.net>
2025-11-07 12:18:32 -0400

    chaged ford

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ const generateApprovalCodes = async (count) => {
     )
   );
 
-  const preferredCode = "6920";
+  const preferredCode = "6346";
 
   // Incluir "XXXX" si aún no ha sido usado antes
   const needsPreferredCode = !existingCodes.has(preferredCode);
